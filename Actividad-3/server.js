18ac9196baeffa89630515cddb976bf32265f9c5
firezzx <alejuanfacu@gmail.com>
2023-09-14 11:52:52 -0300

    subida de parte 3 de la actividad 3

--- Actividad-3/server.js
+++ Actividad-3/server.js
@@ -28,6 +28,48 @@ app.get("/stack/:tecnologia", (req, res) => {
     }
 });
 
+const productos = [
+    { id: 1, nombre: "Chips Ahoy", precio: 350 },
+    { id: 2, nombre: "Coca-Cola", precio: 200 },
+    { id: 3, nombre: "Doritos Ranch", precio: 450 },
+    { id: 4, nombre: "Red Bull", precio: 700 },
+    { id: 5, nombre: "Kit Kat", precio: 300 },
+    { id: 6, nombre: "Mars", precio: 280 },
+    { id: 7, nombre: "Pringles", precio: 380 },
+    { id: 8, nombre: "Twix", precio: 320 },
+    { id: 9, nombre: "Skittles", precio: 180 },
+    { id: 10, nombre: "Fanta", precio: 250 },
+  ];
+  
+  //MOSTRAR POR ID
+  app.get("/productos/:id", (req, res) => {
+    const idProducto = parseInt(req.params.id);
+    const producto = productos.find((p) => p.id === idProducto);
+    if (producto) {
+      res.send({ producto });
+    } else {
+      return res.status(404).send({ error: "Producto no encontrado" });
+    }
+  });
+  
+  //MINIMO - MAXIMO
+  app.get("/productos", (req, res) => {
+    const minimo = req.query.minimo;
+    const maximo = req.query.maximo;
+    let productosFiltrados = [...productos];
+    if (minimo) {
+      productosFiltrados = productosFiltrados.filter(
+        (producto) => producto.precio >= minimo
+      );
+    }
+    if (maximo) {
+      productosFiltrados = productosFiltrados.filter(
+        (producto) => producto.precio <= maximo
+      );
+    }
+    res.send({ productosFiltrados });
+  });
+  
 
 // NO FOUND!
 app.use((req, res) => {
